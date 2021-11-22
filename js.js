1189d48fd73ad152501dc19442eb277c19ec4e41
boris.bulgakov <boris.bulgakov@simpleone.ru>
2021-11-22 23:08:01 +0300

    third commit

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-alert('обоссаный плед')
+let ric = confirm('Не желаете ли обоссать мой плед?');
+let answer = document.querySelector('.dick');
+
+if (ric == true) {
+    answer.textContent = 'Спасибо, что обоссали мой плед';
+} else {
+    answer.textContent = 'Очень зря';
+}
