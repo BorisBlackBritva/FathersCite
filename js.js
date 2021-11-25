587ad376945a7c0e649c4dbde72810276fc7c486
BorisBlackBritva <78314954+BorisBlackBritva@users.noreply.github.com>
2021-11-25 19:46:55 +0300

    переход по ссылке

--- js.js
+++ js.js
@@ -5,4 +5,8 @@ if (ric == true) {
     answer.textContent = 'Спасибо, что обоссали мой плед';
 } else {
     answer.textContent = 'Очень зря';
+    async function hello() 
+        await pause(2000)
+        document.location.href = "https://i.pinimg.com/originals/e5/41/81/e541819251da2680bbad610c79202296.jpg";
+}
 }
