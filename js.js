fefc4d6f04c436e1ddc2bbd2fdf20ff38532c336
boris.bulgakov <boris.bulgakov@simpleone.ru>
2021-11-28 19:24:40 +0300

    mainPage 1

--- js.js
+++ js.js
@@ -1,12 +1,1 @@
-let ric = confirm('Не желаете ли обоссать мой плед?');
-let answer = document.querySelector('.dick');
 
-if (ric == true) {
-    answer.textContent = 'Спасибо, что обоссали мой плед';
-} else {
-    answer.textContent = 'Очень зря';
-    async function hello() 
-        await pause(2000)
-        document.location.href = "https://i.pinimg.com/originals/e5/41/81/e541819251da2680bbad610c79202296.jpg";
-}
-}
